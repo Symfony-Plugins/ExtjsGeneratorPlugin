5bfef1bb890dc17e7736e21e2854270d4b42c890
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-06-23 08:50:55 -0500

    added sfExtjs3Plugin configurations for our ux javascript classes.
    git-svn-id: http://svn.symfony-project.com/plugins/ExtjsGeneratorPlugin/trunk@29939 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/ExtjsGeneratorPluginConfiguration.class.php
+++ config/ExtjsGeneratorPluginConfiguration.class.php
@@ -17,15 +17,15 @@ class ExtjsGeneratorPluginConfiguration extends sfPluginConfiguration
   {
     // Route to xtype-script-getter
     $this->dispatcher->connect('routing.load_configuration', array(
-      'ExtjsGeneratorPluginRouting',
+      'ExtjsGeneratorPluginRouting', 
       'listenToRoutingLoadConfigurationEvent'
     ));
-
+    
     //automatically enable modules
     if(is_array(sfConfig::get('sf_enabled_modules')))
     {
       $moduleArr = array(
-        'ExtjsGeneratorPluginXtypeManager',
+        'ExtjsGeneratorPluginXtypeManager', 
         'IconMgrPreview'
       );
       foreach($moduleArr as $module)
@@ -38,11 +38,11 @@ class ExtjsGeneratorPluginConfiguration extends sfPluginConfiguration
         }
       }
     }
-
+    
     $default_stylesheets = array(
       '/ExtjsGeneratorPlugin/css/ExtjsGeneratorPlugin.css'
     );
-
+    
     $default_javascripts = array(
       '/ExtjsGeneratorPlugin/js/ext-basex.js',  // BaseX/JIT 4.1 library, used for monitoring XHR requests (monitoring credentials) and lazy loading
       '/ExtjsGeneratorPlugin/js/jit.js',  // BaseX/JIT 4.1 library, used for monitoring XHR requests (monitoring credentials) and lazy loading
@@ -50,20 +50,55 @@ class ExtjsGeneratorPluginConfiguration extends sfPluginConfiguration
       '/ExtjsGeneratorPlugin/Ext.ux.IconMgr/Ext.ux.IconMgr.js',  // icon manager extension.  goes first so we can use it anywhere
       '/ExtjsGeneratorPlugin/js/ExtjsGeneratorConstants.js',  // Generator javascript constants
       '/ExtjsGeneratorPlugin/js/Ext.ux.TabCloseMenu.js',  // simple context menu for closing tabs or multiple tabs
-      '/ExtjsGeneratorPlugin/js/Ext.ux.grid.RowActions.js',
+      '/ExtjsGeneratorPlugin/js/Ext.ux.grid.RowActions.js', 
       '/ExtjsGeneratorPlugin/js/Ext.form.TextField.override.js',  //adds reset event
-      '/ExtjsGeneratorPlugin/js/Ext.grid.GridPanel.override.js',
+      '/ExtjsGeneratorPlugin/js/Ext.grid.GridPanel.override.js', 
       '/ExtjsGeneratorPlugin/js/Ext.ux.MessageBox.js',  // adds autohiding info and error message types
-      '/ExtjsGeneratorPlugin/js/Ext.ux.form.MultiSelect.js',
-      '/ExtjsGeneratorPlugin/js/Ext.ux.form.ItemSelector.js',
-      '/ExtjsGeneratorPlugin/js/Ext.ux.form.TwinDateField.js',
-      '/ExtjsGeneratorPlugin/js/Ext.ux.form.TwinComboBox.js',
-      '/ExtjsGeneratorPlugin/js/Ext.ux.form.IsEmptyCheckbox.js',
-      '/ExtjsGeneratorPlugin/js/Ext.ux.grid.CheckColumn.js',
+      '/ExtjsGeneratorPlugin/js/Ext.ux.form.MultiSelect.js', 
+      '/ExtjsGeneratorPlugin/js/Ext.ux.form.ItemSelector.js', 
+      '/ExtjsGeneratorPlugin/js/Ext.ux.form.TwinDateField.js', 
+      '/ExtjsGeneratorPlugin/js/Ext.ux.form.TwinComboBox.js', 
+      '/ExtjsGeneratorPlugin/js/Ext.ux.form.IsEmptyCheckbox.js', 
+      '/ExtjsGeneratorPlugin/js/Ext.ux.grid.CheckColumn.js', 
       '/ExtjsGeneratorPlugin/js/Ext.data.HttpProxy.override.js' // adds setMethod method
     );
-
+    
     sfConfig::set('extjs_gen_default_javascripts', $default_javascripts);
     sfConfig::set('extjs_gen_default_stylesheets', $default_stylesheets);
+    
+    // add support for our javascript ux files to sfExtjs3Plugin
+    sfConfig::set('sf_extjs3_classes', array_merge(array(
+      'TwinDateField' => 'Ext.ux.form.TwinDateField', 
+      'TwinComboBox' => 'Ext.ux.form.TwinComboBox', 
+      'MultiSelect' => 'Ext.ux.form.MultiSelect', 
+      'ItemSelector' => 'Ext.ux.form.ItemSelector', 
+      'IsEmptyCheckbox' => 'Ext.ux.form.IsEmptyCheckbox'
+    ), sfConfig::get('sf_extjs3_classes', array())));
+    
+    sfConfig::set('Ext.ux.form.TwinDateField', array(
+      'class' => 'Ext.ux.form.TwinDateField', 
+      'attributes' => array()
+    ));
+    
+    sfConfig::set('Ext.ux.form.TwinComboBox', array(
+      'class' => 'Ext.ux.form.TwinComboBox', 
+      'attributes' => array()
+    ));
+    
+    sfConfig::set('Ext.ux.form.MultiSelect', array(
+      'class' => 'Ext.ux.form.MultiSelect', 
+      'attributes' => array()
+    ));
+    
+    sfConfig::set('Ext.ux.form.ItemSelector', array(
+      'class' => 'Ext.ux.form.ItemSelector', 
+      'attributes' => array()
+    ));
+    
+    sfConfig::set('Ext.ux.form.IsEmptyCheckbox', array(
+      'class' => 'Ext.ux.form.IsEmptyCheckbox', 
+      'attributes' => array()
+    ));
+  
   }
 }
