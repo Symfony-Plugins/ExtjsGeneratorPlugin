f221f2c2e6303e91d98a61b4c4467d4c818f33b8
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-07-22 09:19:35 -0500

    updated fieldhelp to work with checkboxes and radio buttons.  added a check to not show the help if the plugin is added to an element but no helpText is present.
    git-svn-id: http://svn.symfony-project.com/plugins/ExtjsGeneratorPlugin/trunk@30364 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- web/js/Ext.ux.form.field.plugin.FieldHelp.js
+++ web/js/Ext.ux.form.field.plugin.FieldHelp.js
@@ -6,54 +6,48 @@ Ext.ux.form.field.plugin.FieldHelp = Ext.extend(Object, (function() {
 
   function afterFieldRender() {
 
-    var style = 'background-color:transparent; background-repeat:no-repeat;';
-    // radio
-    if (this.inputType == 'radio') {
-      style += 'font-size:13px; padding-left:11px; display:inline; position:relative;';
-      style += (this.boxLabel != '&#160;') ? 'left:4px;' : 'left:-4px;';
-      style += Ext.isIE ? 'top:1px;' : 'top:2px;';
+    if (this.helpText) {
+      var style = 'background-color:transparent; background-repeat:no-repeat;';
+      // radio and checkbox
+      if ('undefined' != typeof this.checked || this.inputType == 'radio') {
+        style += 'font-size:13px; padding-left:11px; display:inline; position:relative;';
+        style += (this.boxLabel != '&#160;') ? 'left:4px;' : 'left:-4px;';
+        style += Ext.isIE ? 'top:1px;' : 'top:2px;';
 
-      if (!this.entendedHelpText)
-        this.extendedHelpText = this.helpText;
-      this.helpText = '&nbsp;';
-    }
-    // checkbox
-    else if ('undefined' != typeof this.checked) {
-      style += 'font-size:13px; padding-left:11px; display:inline; position:relative;';
-      style += (this.boxLabel != '&#160;') ? 'left:4px;' : 'left:-4px;';
-      style += Ext.isIE ? 'top:1px;' : 'top:2px;';
+        if (!this.entendedHelpText)
+          this.extendedHelpText = this.helpText;
+        this.helpText = '&nbsp;';
+      }
+      // everything else
+      else {
+        style += 'color:#888; font-size:10px; line-height:16px; padding-left:18px;'
+      }
 
-      if (!this.entendedHelpText)
-        this.extendedHelpText = this.helpText;
-      this.helpText = '&nbsp;';
-    }
-    // everything else
-    else {
-      style += 'color:#888; font-size:10px; line-height:16px; padding-left:18px;'
-    }
+      if (!this.wrap) {
+        this.wrap = this.el.wrap({
+          cls : 'x-form-field-wrap'
+        });
+        this.positionEl = this.resizeEl = this.wrap;
+        this.actionMode = 'wrap';
+        this.onResize = this.onResize.createSequence(syncInputSize);
+      }
 
-    if (!this.wrap) {
-      this.wrap = this.el.wrap({
-        cls : 'x-form-field-wrap'
+      var helpEl = this.wrap[this.helpAlign == 'top' ? 'insertFirst' : 'createChild']({
+        cls : Ext.ux.IconMgr.getIcon('help'),
+        style : style,
+        html : this.helpText
       });
-      this.positionEl = this.resizeEl = this.wrap;
-      this.actionMode = 'wrap';
-      this.onResize = this.onResize.createSequence(syncInputSize);
-    }
 
-    var helpEl = this.wrap[this.helpAlign == 'top' ? 'insertFirst' : 'createChild']({
-      cls : Ext.ux.IconMgr.getIcon('help'),
-      style : style,
-      html : this.helpText
-    });
-
-    if (this.extendedHelpText) {
-      Ext.QuickTips.register({
-        target : helpEl,
-        text : this.extendedHelpText,
-        enabled : true
-      });
-      helpEl.applyStyles({cursor:'help'});
+      if (this.extendedHelpText) {
+        Ext.QuickTips.register({
+          target : helpEl,
+          text : this.extendedHelpText,
+          enabled : true
+        });
+        helpEl.applyStyles({
+          cursor : 'help'
+        });
+      }
     }
   }
 
