261040b09c7a923f42955525012a5d5748a2e231
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-10-12 08:55:50 -0500

    fixed bug with routing for export action link.
    git-svn-id: http://svn.symfony-project.com/plugins/ExtjsGeneratorPlugin/trunk@31102 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/ExtjsModule/admin/template/templates/_listaction__export.js.php
+++ data/generator/ExtjsModule/admin/template/templates/_listaction__export.js.php
@@ -1,5 +1,5 @@
 [?php // @object $sfExtjs3Plugin string $className and @object $topToolbar provided
   $topToolbar->methods["_export"] = $sfExtjs3Plugin->asMethod("
-    window.location = '<?php echo $this->getUrlForAction('list') ?>.csv';
+    window.location = '" . url_for('@<?php echo $this->params['route_prefix'] ?>') . "/index.csv';
   ");
 ?]
